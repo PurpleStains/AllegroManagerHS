8bd134acf4d1476763b4b9b5e2ec80900f568dc2
Seweryn <campery11@gmail.com>
2024-04-09 21:15:47 +0200

    Simplified allegro authorize command handler

--- AllegroConnector.Application/AllegroAuthorization/Commands/AuthorizeCommandHandler.cs
+++ AllegroConnector.Application/AllegroAuthorization/Commands/AuthorizeCommandHandler.cs
@@ -10,55 +10,28 @@ namespace AllegroConnector.Application.AllegroAuthorization.Commands
     {
         public async Task Handle(AuthorizeCommand request, CancellationToken cancellationToken)
         {
-            while (true)
+            await Task.Delay(request.Interval * 1000, cancellationToken);
+            var response = await apiClient.GetAccessToken(request.Interval, request.DeviceCode, cancellationToken);
+            AllegroOAuthToken token = null;
+            var result = response.Match(
+                resultValue =>
+                {
+                    token = mapper.Map<AllegroOAuthToken>(resultValue);
+                    token.DateTimeStamp = DateTime.Now;
+                    token.ExpiresIn = DateTime.Now.AddSeconds(resultValue.expires_in);
+                    logger.Information("Successfully gathered access token from Allegro Api");
+                    return resultValue;
+                },
+                error =>
+                {
+                    logger.Error(error.error_description);
+                    return error;
+                });
+
+            if (result.IsSuccess)
             {
-                int interval = request.Interval;
-                await Task.Delay(interval * 1000, cancellationToken);
-                var response = await apiClient.GetAccessToken(request.Interval, request.DeviceCode, cancellationToken);
-                AllegroOAuthToken token = null;
-                string errorMessage = string.Empty;
-                response.Match(
-                    resultValue =>
-                    {
-                        token = mapper.Map<AllegroOAuthToken>(resultValue);
-                        token.DateTimeStamp = DateTime.Now;
-                        token.ExpiresIn = DateTime.Now.AddSeconds(resultValue.expires_in);
-                        return resultValue;
-                    },
-                    error =>
-                    {
-                        errorMessage = error.error_description;
-                        return error;
-                    });
-
-
-
-                if (response.IsSuccess)
-                {
-                    await repository.AddAsync(token);
-                    await repository.Commit();
-                    return;
-                }
-
-
-                if (errorMessage == "authorization_pending")
-                {
-                    logger.Information("Authorization pending...");
-                    continue;
-                }
-                else if (errorMessage == "slow_down")
-                {
-                    interval += interval;
-                    continue;
-                }
-                else if (errorMessage == "access_denied")
-                {
-                    throw new Exception("Access denied.");
-                }
-                else if (errorMessage == "Invalid device code")
-                {
-                    throw new Exception("Invalid device code.");
-                }
+                await repository.AddAsync(token);
+                await repository.Commit();
             }
         }
     }
