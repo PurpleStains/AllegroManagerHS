b029b295d707796880d1e4f8e6dd6ec42b8ff4c5
Seweryn <campery11@gmail.com>
2024-06-10 22:19:08 +0200

    Added tests to check invalid behavior of authorization handlers

--- AllegroManager.AllegroConnector.Application/AuthorizationTest.cs
+++ AllegroManager.AllegroConnector.Application/AuthorizationTest.cs
@@ -26,8 +26,7 @@ namespace AllegroManager.AllegroConnector.Application
         public async Task Handle_Should_Return_SuccessfullyGetCodeMessage_When_Correct_Parameters()
         {
             _allegroOAuthService.Setup(x => x.GetCode())
-                               .Returns(() =>
-                                   Task.FromResult(Result.Ok(new AuthDeviceOAuth())));
+                               .Returns(() => Task.FromResult(Result.Ok(new AuthDeviceOAuth())));
             var command = new GetCodeCommand();
             var commandHandler = new GetCodeCommandHandler(_allegroOAuthService.Object);
 
@@ -35,6 +34,22 @@ namespace AllegroManager.AllegroConnector.Application
 
             result.IsSuccess.Should().BeTrue();
             result.Errors.Should().HaveCount(0);
+        }
+
+        [Test]
+        public async Task Handle_Should_Return_FailedGetCodeMessage_When_Incorrect_Parameters()
+        {
+            _allegroOAuthService.Setup(x => x.GetCode())
+                               .Returns(() =>Task.FromResult(Result.Fail<AuthDeviceOAuth>("Something went wrong")));
+
+            var command = new GetCodeCommand();
+            var commandHandler = new GetCodeCommandHandler(_allegroOAuthService.Object);
+
+            var result = await commandHandler.Handle(command, CancellationToken.None);
+
+            result.IsSuccess.Should().BeFalse();
+            result.Errors.Should().HaveCount(1);
+            result.Errors.Should().ContainEquivalentOf(new Error("Failed to get device code"));
         }
 
         [Test]
@@ -53,6 +68,26 @@ namespace AllegroManager.AllegroConnector.Application
 
             result.IsSuccess.Should().BeTrue();
             result.Errors.Should().HaveCount(0);
+        }
+
+        [Test]
+        public async Task Handle_Should_Return_FailedAuthorizedResponseMessage_When_Api_Failed()
+        {
+            _allegroOAuthService.Setup(x => x.GetAccessToken(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                                   .Returns(() => Task.FromResult(Result.Fail<AuthResponse>("Not authorized")));
+
+
+            _tokenRepository.Setup(x => x.AddAsync(It.IsAny<AllegroOAuthToken>())).Returns(Task.CompletedTask);
+            _mapper.Setup(x => x.Map<AllegroOAuthToken>(It.IsAny<object>())).Returns(new AllegroOAuthToken());
+
+            var command = new AuthorizeCommand(It.IsAny<int>(), It.IsAny<string>());
+            var commandHandler = new AuthorizeCommandHandler(_tokenRepository.Object, _allegroOAuthService.Object, _mapper.Object);
+
+            var result = await commandHandler.Handle(command, CancellationToken.None);
+
+            result.IsSuccess.Should().BeFalse();
+            result.Errors.Should().HaveCount(1);
+            result.Errors.Should().ContainEquivalentOf(new AuthorizationError("Not authorized"));
         }
     }
 }
