26796ad79838f8847d1ccb7bdff5f72d4bc40843
Seweryn <campery11@gmail.com>
2024-09-14 11:05:00 +0200

    Init EventBus in building blocks

---
+++ AllegroConnector.BuildingBlocks.Infrastructure/EventBus/IEventsBus.cs
@@ -0,0 +1,11 @@
+﻿namespace AllegroConnector.BuildingBlocks.Infrastructure.EventBus
+{
+    public interface IEventsBus
+    {
+        Task Publish<T>(T @event) where T : IntegrationEvent;
+
+        void Subscribe<T>(IIntegrationEventHandler<T> handler) where T : IntegrationEvent;
+
+        void StartConsuming();
+    }
+}
