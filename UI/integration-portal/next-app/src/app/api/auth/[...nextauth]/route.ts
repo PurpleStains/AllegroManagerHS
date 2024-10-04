76e5fd444d2d06b64bf2578d907339cd63515195
Seweryn.Sergjeiusz <s.sergjeiusz@godeltech.com>
2024-10-04 10:10:15 +0200

    Added non null assertion to route.ts

--- UI/integration-portal/next-app/src/app/api/auth/[...nextauth]/route.ts
+++ UI/integration-portal/next-app/src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,12 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import KeycloakProvider from "next-auth/providers/keycloak"
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
   providers: [
     KeycloakProvider({
-      clientId: process.env.KEYCLOAK_CLIENT_ID,
-      clientSecret: process.env.KEYCLOAK_CLIENT_SECRET,
-      issuer: process.env.KEYCLOAK_ISSUER
+      clientId: process.env.KEYCLOAK_CLIENT_ID!,
+      clientSecret: process.env.KEYCLOAK_CLIENT_SECRET!,
+      issuer: process.env.KEYCLOAK_ISSUER!
     })
   ]
 }
